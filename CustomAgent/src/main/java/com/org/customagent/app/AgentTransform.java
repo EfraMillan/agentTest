5c95cefd1a8e78e5a9860cb20a7611dfe79f2780
elias <elias.millan10@gmail.com>
2018-08-12 23:30:16 -0300

    more test

--- CustomAgent/src/main/java/com/org/customagent/app/AgentTransform.java
+++ CustomAgent/src/main/java/com/org/customagent/app/AgentTransform.java
@@ -10,6 +10,8 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.NotFoundException;
+import javassist.bytecode.AttributeInfo;
+import javassist.bytecode.LocalVariableAttribute;
 
 public class AgentTransform implements ClassFileTransformer {
 
@@ -19,37 +21,39 @@ public class AgentTransform implements ClassFileTransformer {
 		System.out.println("class : " + className);
 
 		try {
-			String strClazz = className.replaceAll("/", ".");
-			final ClassPool classPool = ClassPool.getDefault();
-			final CtClass clazz = classPool.get(strClazz);
+			if (className.equals("com/org/testagent/app/App")) {
+				String strClazz = className.replaceAll("/", ".");
+				final ClassPool classPool = ClassPool.getDefault();
+				final CtClass clazz = classPool.get(strClazz);
 
-			System.out.println("---------------" + clazz.getName() + "-------------------------------");
-			System.out.println("Fields : " + clazz.getFields().toString());
-			System.out.println("package : " + clazz.getPackageName());
-			System.out.println("Simple name : " + clazz.getSimpleName());
-			
-	        final CtMethod[] existingMethods = clazz.getDeclaredMethods();
-            for (int ctr = 0; ctr < existingMethods.length; ctr++)
-            {
-                final CtMethod method = existingMethods[ctr];
-             System.out.println("Method long name : "+ method.getLongName());
-             System.out.println("Method name : "+ method.getName());
-             System.out.println("Method signature : "+ method.getSignature());
-             System.out.println("Method  parameters : "+ method.getParameterTypes());
-             			     CtClass[] parameters = method.getParameterTypes(); 
-             			    for (int i = 0; i < parameters.length; i++) {
-             			       final CtClass param = parameters[i];
-             			      System.out.println("param class  : " + param.getName());
-             			    }
-            }
-			
-			System.out.println("----------------------------------------------------");
+				System.out.println("---------------" + clazz.getName() + "-------------------------------");
+				System.out.println("Fields : " + clazz.getFields().toString());
+				System.out.println("package : " + clazz.getPackageName());
+				System.out.println("Simple name : " + clazz.getSimpleName());
 
-			byte[] byteCode = clazz.toBytecode();
+				final CtMethod[] existingMethods = clazz.getDeclaredMethods();
+				for (int ctr = 0; ctr < existingMethods.length; ctr++) {
+					final CtMethod method = existingMethods[ctr];
+					AttributeInfo localVar = method.getMethodInfo().getAttribute(javassist.bytecode.LocalVariableAttribute.tag);
+					System.out.println("Method long name : " + method.getLongName());
+					System.out.println("Method name : " + method.getName());
+					System.out.println("Method signature : " + method.getSignature());
+					System.out.println("Method  parameters : " + method.getParameterTypes());
+					CtClass[] parameters = method.getParameterTypes();
+					for (int i = 0; i < parameters.length; i++) {
+						final CtClass param = parameters[i];
+						System.out.println("param class  : " + param.getName());
+					}
+				}
 
-			clazz.detach();
+				System.out.println("----------------------------------------------------");
 
-			return byteCode;
+				byte[] byteCode = clazz.toBytecode();
+
+				clazz.detach();
+
+				return byteCode;
+			}
 
 		} catch (NotFoundException | IOException | CannotCompileException e) {
 			System.out.println("Error : " + e.getMessage());
